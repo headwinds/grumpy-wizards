12ee30196cc5a5747cabfeaa77af0ab1683d213e
Adrian Hall <adrian@shellmonger.com>
2016-01-13 19:43:09 -0800

    Added react-tap-event-plugin

--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import Chrome from './components/Chrome.jsx';
 import ClientLogger from './lib/logger';
 
@@ -9,6 +10,10 @@ require('./app.scss');
 let logger = new ClientLogger(__filename);
 logger.info('Booting Application');
 
+// support tap events
+injectTapEventPlugin();
+
+// render the page
 ReactDOM.render(
     <Router history={browserHistory}>
         <Route path="/" component={Chrome}>
