5bf17d5e7721c7dfe38f8d4a71fda235969dff85
Adrian Hall <adrian@shellmonger.com>
2016-01-27 16:53:45 -0800

    Added async support to the redux store

--- client/redux/store.js
+++ client/redux/store.js
@@ -1,4 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createLogger from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import promiseMiddleware from 'redux-promise';
 import { requestAuthInfo } from './actions';
 import reducer from './reducers';
 
@@ -9,10 +12,15 @@ const initialState = {
     leftMenuVisibility: false
 };
 
-let store = createStore(reducer, initialState);
+const loggerMiddleware = createLogger();
+const reduxStore = applyMiddleware(
+    thunkMiddleware,
+    promiseMiddleware,
+    loggerMiddleware
+)(createStore);
+const store = reduxStore(reducer, initialState);
 
 // Dispatch the initial action
-let requestAction = requestAuthInfo();
-requestAction(store.dispatch);
+store.dispatch(requestAuthInfo());
 
 export default store;
